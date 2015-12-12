d336dfda7c7cb9179f3be0eb605c67b24af85056
Kevin Glick <kevin@grizzdev.com>
2015-12-11 20:28:18 -0800

    Matching live site; fixed short_description on Product form

--- resources/views/cms/products/form.blade.php
+++ resources/views/cms/products/form.blade.php
@@ -91,9 +91,7 @@ foreach ($product->categories as $cat) {
 				<div class="col-xs-12 col-sm-6">
 					<div class="form-group has-feedback">
 						{!! Form::label('short_description', 'Short Description', ['class' => 'control-label']) !!}
-						@include('cms.includes.editor_toolbar', ['target' => 'short_description'])
-						<div class="editor" id="short_description" contenteditable="true">{!! $product->short_description !!}</div>
-						<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+						{!! Form::textarea('short_description', null, ['class' => 'form-control editor', 'required']) !!}
 					</div>
 				</div>
 			</div>
